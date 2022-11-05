5503529d564b080c84eca3578152cd91a464b258
Charbel Maroun <charbel-maroun@hotmail.com>
2022-11-04 20:33:59 -0400

    commit tree 284ac14a12e062b3f1f6613d2d1cfea60798678e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(driverNames) {
+    return driverNames.slice(0,2);
+};
+
+const returnLastTwoDrivers = function(driverNames) {
+
+    return driverNames.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+    return function(fare){
+        return fare * num
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, selectingDrivers){
+
+    return selectingDrivers(arrayOfDrivers);
+
+    
+
+}
